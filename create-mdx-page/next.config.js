a39109e6e8f0697338288331a7f3ab30cd458e64
Yu Watanabe <watanabe.yu.1203@gmail.com>
2022-12-19 10:40:53 +0900

    Add MDX to next.config.js

--- create-mdx-page/next.config.js
+++ create-mdx-page/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+module.exports = withMDX({
+  // デフォルトでは .tsx, .ts, .jsx, .js なので、
+  // .mdと.mdxファイルからもページを生成できるようにする。
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'md', 'mdx'],
 
-module.exports = nextConfig
+  //最初から設定されているオプション
+  reactStrictMode: true,
+})
