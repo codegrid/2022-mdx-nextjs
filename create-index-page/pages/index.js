4c929681e1f356c9564c48c2a9890aa3f1b59ad6
Yu Watanabe <watanabe.yu.1203@gmail.com>
2023-01-05 21:34:33 +0900

    Add getStaticProps() to index.js

--- create-index-page/pages/index.js
+++ create-index-page/pages/index.js
@@ -1,10 +1,25 @@
-import Head from 'next/head'
+import { promises as fs } from 'node:fs'
 
+import Head from 'next/head'
 import News from '../contents/news.mdx'
+
+export const getStaticProps = async () => {
+  const dataDir = 'articles'
+  const fileNames = await fs.readdir(dataDir)
+  const posts = []
+  for (const fileName of fileNames) {
+    const { meta } = await import(`../articles/${fileName}`)
+    const slug = fileName.replace(/\.mdx$/, '')
+    posts.push({ slug: slug, meta: meta })
+  }
+  return {
+    props: { posts },
+  }
+}
 
 const userName = 'ピクセル グリオ'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <>
       <Head>
@@ -15,6 +30,15 @@ export default function Home() {
       </Head>
       <main>
         <h2>新着記事</h2>
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.slug}>
+                <a href={post.slug}>{post.meta.title}</a>
+              </li>
+            )
+          })}
+        </ul>
         <h2>お知らせ</h2>
         <News userName={userName} />
       </main>
